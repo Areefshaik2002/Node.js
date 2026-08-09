291b5cfb1a830311ebb70ddbb26bd6f5218aed11
Areef shaik <126571035+Areefshaik2002@users.noreply.github.com>
2026-08-09 12:01:07 +0530

    updated code

--- index.ts
+++ index.ts
@@ -5,3 +5,21 @@ import * as uuid from 'uuid'
 console.log(format(new Date(), 'yyyy/MM/dd HH:mm:ss'))
 
 console.log(uuid.v4());
+
+/////
+
+import logEvents from './logEvents.js'
+import EventEmitter from 'events'
+
+class MyEmitter extends EventEmitter {};
+
+//Initialize
+const myEmitter = new MyEmitter();
+
+//add listner
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    //emit event
+    myEmitter.emit('log', 'log event emitted');
+}, 2000)
--- logEvents.ts
+++ logEvents.ts
@@ -3,8 +3,24 @@ import * as uuid from 'uuid'
 import fs from 'fs'
 import path from 'path'
 import * as fsPromises from 'node:fs/promises'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-console.log(format(new Date(), 'yyyy/MM/dd HH:mm:ss'))
+const logEvents = async (message:String) => {
+    const dateTime = `${format(new Date(), 'yyyy/MM/dd HH:mm:ss')}`;
+    const logitem = `${dateTime} \tuuid: ${uuid.v4()}  \tmesssage: ${message}\n`;
+    console.log(logitem);
+    try {
+        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'));
+        }
+        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logitem);
+    } catch (err) {
+        console.error(err);
+    } 
+}
 
-console.log(uuid.v4());
+export default logEvents;
